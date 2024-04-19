6a5fb6ea198085782e3348c041a611f5314f648d
kh4rg0sh <36923816+MmukulKhedekar@users.noreply.github.com>
2024-04-19 12:34:54 +0530

    persistent changes

--- src/components/CodeEditor.js
+++ src/components/CodeEditor.js
@@ -1,21 +1,34 @@
 import Editor from '@monaco-editor/react';
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 const CodeEditor = () => {
     const editorRef = useRef();
-    const [value, setValue] = useState('');
+    const [value, setValue] = useState(() => {
+        const storedValue = localStorage.getItem('code');
+        return storedValue ? storedValue : '';
+    });
+
+    useEffect(() => {
+        const handleBeforeUnload = () => {
+            localStorage.setItem('code', value);
+        };
+        window.addEventListener('beforeunload', handleBeforeUnload);
+        return () => {
+            window.removeEventListener('beforeunload', handleBeforeUnload);
+        };
+    }, [value]);
 
     const onMount = (editor) => {
         editorRef.current = editor;
         editor.focus();
-    }
+    };
 
     return (
         <Editor
             height="100vh"
             theme="vs-dark"
             defaultLanguage="cpp"
-            defaultValue=""
+            defaultValue={value}
             value={value}
             onMount={onMount}
             onChange={(value) => setValue(value)}
